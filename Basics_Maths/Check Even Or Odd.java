76acf8c762f2069d8f5e735c9f06073f1ef64819
Joshi <66480820+Joshi-Adi@users.noreply.github.com>
2025-08-20 01:18:25 +0530

    Update Check Even Or Odd.java

--- Basics_Maths/Check Even Or Odd.java
+++ Basics_Maths/Check Even Or Odd.java
@@ -1,1 +1,11 @@
-
+class GfG {
+    public static void main(String[] args)
+    {
+      Scanner sc=new Scanner(System.in)
+        int n = sc.nextInt();
+        if ((n & 1) == 0)   // taking bitwise and of n with 1 
+            System.out.println("Even");
+        else
+            System.out.println("Odd");
+    }
+}
